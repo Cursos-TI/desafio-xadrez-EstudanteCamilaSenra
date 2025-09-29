f9e2a14bc3400e8a46dcfccb781f91790832539b
Camila Senra <202503601021@alunos.estacio.br>
2025-09-29 15:46:38 -0300

    Nível Novato
    Realização do nível novato.

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
+#include <locale.h>
+#include <windows.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    //Correção de problemas de acentuação e caracteres especiais.
+    UINT CPAGE_UTF8 = 65001;
+    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+    SetConsoleOutputCP(CPAGE_UTF8);
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Move-se cinco casas na diagonal para cima e à direita. 
+    int bispo = 1;
+
+    while(bispo <=5){
+        printf("Bispo se move para cima e direita!\n");
+        bispo ++;
+    }
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Move-se em linha reta horizontalmente cinco casas para a direita.
+    int torre = 1;
+    do {
+        printf("Torre se move para a direita!\n");
+        torre ++;
+    } while (torre <= 5);
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Move-se oito casas para a esquerda.
+    int rainha;
+    for(rainha = 1; rainha <=8; rainha ++){
+        printf("Rainha se move para a esquerda!\n");
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
